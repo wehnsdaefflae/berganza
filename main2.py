c7d4698b012457b27d442d0e4dd803285c425131
Mark <wernsdorfer@gmail.com>
2022-03-14 09:19:00 +0100

    added mains

---
+++ main2.py
@@ -0,0 +1,6 @@
+from transformers import pipeline
+
+generator = pipeline(task="text-generation")
+
+generator("Three Rings for the Elven-kings under the sky, Seven for the Dwarf-lords in their halls of stone")
+
